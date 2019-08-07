24415a262473d2a1e577e0035b9ebd9ded501a18
Mya Mya Kyaing <myamyakyaing.mmk1996@gmail.com>
2019-08-07 10:28:00 +0630

    Initial Commit

---
+++ app/src/main/java/com/example/teamproject/LoginActivity.kt
@@ -0,0 +1,24 @@
+package com.example.teamproject
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        super.onCreate(savedInstanceState)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        setContentView(R.layout.activity_login)
+        btnLogin.setOnClickListener {
+            var  intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
